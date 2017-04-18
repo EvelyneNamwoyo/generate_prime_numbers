cc82f29f7e9b8df7901a47a928df15a136948437
Evelyne <namwoyoeve@gmail.com>
2017-04-18 11:38:25 +0300

    added correted test to check for invalid input

--- prime_number_test.py
+++ prime_number_test.py
@@ -2,16 +2,16 @@ from bootcamp import prime_nums
 import unittest
 class GeneratePrimeNumbersTestCases(unittest.TestCase):
 	def test_for_negative_input(self):
-		result=prime_nums(6)
+		result=prime_nums(-6)
 		self.assertEqual(result,'Cannot generate prime numbers for negative integers')
 	def test_for_invalid_input(self):
 		with self.assertRaises(ValueError):
-			result=prime_nums(6)
+			result=prime_nums('h')
 	def test_integer_one(self):
-		result=prime_nums(1)
+		result=prime_nums(0)
 		self.assertEqual(result,'Zero or one cannot be a prime number' )
 	def test_integer_zero(self):
-		result=prime_nums(0)
+		result=prime_nums(1)
 		self.assertEqual(result,'Zero or one cannot be a prime number')
 	def test_returns_a_list_of_generated_prime_numbers(self):
 		result=prime_nums(10) 
